c028221f798b8d95ef520eb036796dd393935f69
Anastasia Lukashyna <L_A_A_93@mail.ru>
2020-06-05 20:55:54 +0300

    Updates

---
+++ precache-manifest.af362cfbacfa9fb0254c5e4658d2ad5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294c4293006ba15719503126824f81f0",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "1b4261c9f97fc822d113",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.53f4358f.chunk.css"
+  },
+  {
+    "revision": "89bdd02158918fd3fb8a",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.80bae8ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.80bae8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4261c9f97fc822d113",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.11ae8bd9.chunk.js"
+  },
+  {
+    "revision": "86e9a7fe0c1479afab58",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.2d3ded43.js"
+  },
+  {
+    "revision": "edccec0ceef2faf99a6d961fb0cf5b98",
+    "url": "/goit-react-hw-06-phonebook/static/media/delete.edccec0c.png"
+  }
+]);
